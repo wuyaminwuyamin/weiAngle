07dfa750adfa06b7846aa4775a25079cb17e33b3
youmm <youmm@tongdaohui.com>
2017-03-23 18:16:18 +0800

    阮千军
    3.23工作完成,头像上传完成

--- Weitianshi/pages/my/baseInfo/baseInfo.js
+++ Weitianshi/pages/my/baseInfo/baseInfo.js
@@ -1,25 +1,39 @@
 // pages/my/baseInfo/baseInfo.js
 Page({
-  data:{
-    avatar:"",
-    name:"张三",
-    mobile:18762670539,
-    identity:"请选择",
-    company:"微天使",
-    career:"投资经理",
-    email:"morganfly@126.com",
-    describe:""
+  data: {
+    avatar: "",
+    name: "张三",
+    mobile: 18762670539,
+    identity: "请选择",
+    company: "微天使",
+    career: "投资经理",
+    email: "morganfly@126.com",
+    describe: "",
+    image: "http://wx.qlogo.cn/mmopen/vi_32/bTicibYm55RdTibZEHwic9thpr6MPWKI9ldicEdL7Ep2RkMFXHia3N6yapviaibVkQsib3SiaqNaSayFh0iajb6QgE9PcNZlA/0"
   },
-  onLoad:function(options){
-   
+  onLoad: function (options) {
+
   },
-  onShow:function(){
-  
+  onShow: function () {
+
   },
-  save:function(){
+  // 保存
+  save: function () {
     console.log(1)
     wx.redirectTo({
       url: '../myInfoEdit/myInfoEdit',
     })
-  }
+  },
+  // 添加图片
+  chooseImage: function () {
+    var that = this
+    wx.chooseImage({
+      count: 1,
+      success: function (res) {
+        that.setData({
+          image: res.tempFilePaths
+        })
+      }
+    })
+  },
 })
