ab9e85e1f9792b64057c774c50933a1a7a2fd371
morganfly <786485758@qq.com>
2017-04-09 22:48:52 +0800

    阮千军
    开始修改my页面

--- Weitianshi/pages/network/network.js
+++ Weitianshi/pages/network/network.js
@@ -8,7 +8,7 @@ Page({
     var that = this;
     var user_id = wx.getStorageSync('user_id')
     that.setData({
-      user_id:user_id
+      user_id: user_id
     })
     console.log(user_id)
     if (user_id) {
@@ -37,11 +37,44 @@ Page({
     }
   },
   myCard: function () {
-    wx.navigateTo({
-      url: '../my/cardEdit/cardEdit',
+    var that = this;
+    var user_id = this.data.user_id;
+    //获取用户信息
+    wx.request({
+      url: url + '/api/user/getUserAllInfo',
+      data: {
+        user_id: user_id
+      },
+      method: 'POST',
+      success: function (res) {
+        var name = res.data.user_info.user_real_name;
+        var mobile = res.data.user_info.user_real_mobile;
+        var career = res.data.user_info.user_company_career;
+        var company = res.data.user_info.user_company_name
+        if (name != '' && mobile != '' && career != '' && company != '') {
+          wx.navigateTo({
+            url: '../my/showPage/showPage?user_id=' + user_id,
+          })
+        } else {
+          wx.showModal({
+            title: "提示",
+            content: "请先完善个人信息",
+            success: function () {
+              wx.navigateTo({
+                url: '../my/cardEdit/cardEdit',
+              })
+            }
+          })
+        }
+      },
+      fail: function (res) {
+        wx.showToast({
+          title: "对不起没有获取到您的个人信息"
+        })
+      },
     })
   },
-  bindUserInfo:function(){
+  bindUserInfo: function () {
     wx.navigateTo({
       url: '../myProject/personInfo/personInfo',
     })
