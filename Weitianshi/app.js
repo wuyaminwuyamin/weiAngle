d5c60b63c7b5014faf2c9cf89d0291477f2acc05
youmm <youmm@tongdaohui.com>
2017-05-10 19:29:22 +0800

    阮千军
    app.js改到了一半

--- Weitianshi/app.js
+++ Weitianshi/app.js
@@ -9,11 +9,94 @@ App({
         // console.log(options.scene);
     },
     onShow: function () {
-       
+
     },
     onError: function (msg) {//小程序发生脚本错误,或者api调用失败时,会触发onError,并带上错误信息
         console.log(msg)
     },
+    //进入页面判断是否有open_session
+    loginPage: function (cb) {
+        var that = this;
+        if (this.globalData.open_session) {
+            var timeNow = Date.now();
+            var session_time = this.globalData.session_time;
+            var differenceTime = timeNow - session_time;
+            if (differenceTime > 432000000) {
+                getSession(timeNow)
+            } else {
+                typeof cb == "function" && cb(this.globalData.user_id)
+            }
+        } else {
+            getSession()
+        }
+    },
+
+    //获取open_session
+    getSession() {
+        //获取code
+        wx.login({
+            success: function (login) {
+                var code = login.code;
+                that.globalData.code = code;
+                //获取encryptedData和iv
+                wx.getUserInfo({
+                    success: function (res) {
+                        console.log("这里是wx.getUserInfo")
+                        console.log(res)
+                        that.globalData.userInfo = res.userInfo;
+                        that.globalData.encryptedData = res.encryptedData;
+                        that.globalData.iv = res.iv;
+                        typeof cb == "function" && cb(that.globalData.userInfo);
+                        wx.request({
+                            url: 'https://dev.weitianshi.cn/api/wx/returnOauth',
+                            data: {
+                                code: code,
+                                encryptedData: res.encryptedData,
+                                iv: res.iv
+                            },
+                            method: 'POST',
+                            success: function (res) {
+                                console.log("这里是获取到encryptedData,iv后调用returnOauth")
+                                console.log(res);
+                                //在globalData里存入open_session,session_time,user_id;
+                                that.globalData.open_session = res.data.open_session;
+                                that.globalData.session_time = Date.now();
+                                that.globalData.user_id = res.data.user_id;
+                                console.log(that.globalData.session_time)
+                                wx.setStorageSync("user_id", res.data.user_id)
+                            },
+                            fail: function () {
+                                console.log("调用returnOauth失败")
+                            }
+                        })
+                    },
+                    fail: function (res) {
+                        wx.request({
+                            url: 'https://dev.weitianshi.cn/api/wx/returnOauth',
+                            data: {
+                                code: code,
+                            },
+                            method: 'POST',
+                            success: function (res) {
+                                console.log("这里是没获取到encryptedData,iv后调用returnOauth")
+                                console.log(res);
+                                //在globalData里存入open_session,session_time,user_id;
+                                that.globalData.open_session = res.data.open_session;
+                                that.globalData.session_time = Date.now();
+                                that.globalData.user_id = res.data.user_id;
+                                console.log(that.globalData.session_time)
+                                wx.setStorageSync("user_id", res.data.user_id)
+                            },
+                            fail: function () {
+                                console.log("向后台发送信息失败")
+                            },
+                        })
+                    },
+                })
+            }
+        })
+    },
+
     //进行授权验证
     getUserInfo: function (cb) {
         var that = this;
@@ -45,7 +128,7 @@ App({
                             //如果已经存在session_time就进行比较,如果不没有就建一个session_time;
                             if (that.globalData.session_time) {
                                 var timeNow = new (Date.now())
-                                console.log(that.globalData.session_time,timeNow)
+                                console.log(that.globalData.session_time, timeNow)
                             } else {
                                 that.checkLogin(that);
                             }
@@ -55,6 +138,7 @@ App({
             })
         }
     },
+
     //登录状态维护
     checkLogin: function (that) {
         var code = that.globalData.code;
