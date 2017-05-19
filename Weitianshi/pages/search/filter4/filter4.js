372861d9cad47ab8609a6d053ae4c39fc76f1e4e
youmm <youmm@tongdaohui.com>
2017-05-19 19:14:44 +0800

    阮千军
    1.把标签选中样式提到了app.wxss,并删除了各个页面的wxss里的相关样式代码
    2.封装了多选标签的模版,但是选中相关的效果还没封装进去

--- Weitianshi/pages/search/filter4/filter4.js
+++ Weitianshi/pages/search/filter4/filter4.js
@@ -1,8 +1,5 @@
-// var rqj = require('../../Template/Template.js')
 var app = getApp();
-// 所属领域
 var url = app.globalData.url;
-var initialArr = {};//初始数组
 Page({
   data: {
     industryData: [],
@@ -10,79 +7,36 @@ Page({
     industryName: [],
     stageData :[],
     stageId:[],
-    stageName:[]
+    stageName:[],
+    industryTags:{
+        tagsData:"",
+        bindEvent:"industryChoose"
+    }
   },
 
   onLoad: function (options) {
     var that = this;
     var options = options;
     var industryData = wx.getStorageSync('industry');//获取所属领域的全部信息
+    var industryTags=this.data.industryTags;
     var stageData = wx.getStorageSync('stage');//获取轮次信息
-    // console.log(stageData);
-    var stageData_name = [];
-    var stageData_id = [];
-    var stage
-    for (let i = 0; i < stageData.length; i++) {
-      stageData_name.push(stageData[i].stage_name);
-      console.log(stageData[i].stage_name);
-      stageData_id.push(stageData[i].stage_id)
-    }
+
+    console.log(industryData,stageData)
+
+    industryTags.tagsData=industryData;
     that.setData({
       industryData : industryData,
-      stageData: stageData
+      stageData: stageData,
+      industryTags:industryTags
     })
   },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
+//领域选择
+industryChoose(e){
+    console.log(1)
+    console.log(e.currentTarget.dataset)
+},
+ // 开启了下拉刷新
   onPullDownRefresh: function () {
-    // console.log("开启了下拉刷新");
     wx.stopPullDownRefresh()
   },
-  onReachBottom: function () {
-
-  },
-
-  onShareAppMessage: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  }
 })
