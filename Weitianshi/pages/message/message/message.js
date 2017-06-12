b30ded0cdfb5f2bb2d1c5605932f1b987d78d06e
youmm <youmm@tongdaohui.com>
2017-06-12 13:46:52 +0800

    阮千军
    1.封装了消息列表模版

--- Weitianshi/pages/message/message/message.js
+++ Weitianshi/pages/message/message/message.js
@@ -4,105 +4,42 @@ var url = app.globalData.url;
 var url_common = app.globalData.url_common;
 // pages/message/message/message.js
 Page({
-  data: {
-    item: [{
-      message_name: '',
-      count: ''
-    }]
-  },
-  beAddedContacts: function (e) {
-    wx.navigateTo({
-      url: '/pages/message/beAddedContacts/beAddedContacts',
-    })
-  },
-  onLoad: function (options) {
-    var that = this;
-    var user_id = wx.getStorageSync('user_id');
-    wx.request({
-      url: url_common + '/api/message/messageType',
-      data: {
-        user_id: user_id
-      },
-      method: 'POST',
-      success: function (res) {
-        console.log(res)
-        var data = res.data.data;
-        var dataLength = data.length - 1;
-        var message_name = data[dataLength].message_name;
-        var count = data[dataLength].count;
-        data.forEach((x) => {
-          message_name = x.message_name;
-          count = x.count;
+    data: {
+        messageList: [
+            {
+                event: "beAddedContacts",
+                iconPath: "/img/img-personMessage.png",
+                message_name: "人脉申请",
+                count: 5
+            }
+        ]
+    },
+    beAddedContacts: function (e) {
+        wx.navigateTo({
+            url: '/pages/message/beAddedContacts/beAddedContacts',
         })
-        that.setData({
-          message_name: message_name,
-          count: count
+    },
+    onLoad: function (options) {
+        var that = this;
+        var user_id = wx.getStorageSync('user_id');
+        var messageList = this.data.messageList;
+        wx.request({
+            url: url_common + '/api/message/messageType',
+            data: {
+                user_id: user_id
+            },
+            method: 'POST',
+            success: function (res) {
+                console.log(res)
+                var data = res.data.data;
+                data.forEach((x, index) => {
+                    messageList[index].message_name = x.message_name;
+                    messageList[index].count = x.count
+                })
+                that.setData({
+                    messageList: messageList
+                })
+            }
         })
-      }
-    })
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  }
+    },
 })
--- Weitianshi/pages/userDetail/networkDetail/networkDetail.js
+++ Weitianshi/pages/userDetail/networkDetail/networkDetail.js
@@ -3,198 +3,205 @@ var app = getApp();
 var url = app.globalData.url;
 var url_common = app.globalData.url_common;
 Page({
-  data: {
-    integrity: 30,
-    resourcesIndex: 9.9,
-    user: "",
-    tel: 0,
-    telephone: 0,
-    blue: -1
-  },
-  onLoad: function (options) {
-    var that = this
-    console.log(options);
-    var user_id = options.id;
-    var view_id = wx.getStorageSync('user_id');
-    that.setData({
-      user_id: user_id,
-    })
-    //分享至群打点准备
-    wx.showShareMenu({
-      withShareTicket: true,
-    })
-
-    //用戶的个人信息
-    wx.request({
-      url: url + '/api/user/getUserAllInfo',
-      data: {
-        share_id: 0,
-        user_id: user_id,
-        view_id: view_id,
-      },
-      method: 'POST',
-      success: function (res) {
-        console.log(res)
-        var user = res.data.user_info;
-        var invest = res.data.invest_info;
-        var resource = res.data.resource_info;
-        var project_info = res.data.project_info;
-        var invest_case = res.data.invest_case;
-        var tel = user.user_mobile;
-        var button_type = res.data.button_type;
-        if (button_type == 0) {
-          console.log("是好友或者是我")
-        } else if (button_type == 1) {
-          console.log("我分享出去的名片")
-        } else if (button_type == 2) {
-          console.log("正常添加方式")
-        }
-        if (tel.indexOf("*") != -1) {
-          that.setData({
-            blue: 1
-          })
-        }
-        // console.log(that.data.blue)
+    data: {
+        integrity: 30,
+        resourcesIndex: 9.9,
+        user: "",
+        tel: 0,
+        telephone: 0,
+        blue: -1,
+        a: 1
+    },
+    onLoad: function (options) {
+        var that = this
+        console.log(options);
+        var user_id = options.id;
+        var view_id = wx.getStorageSync('user_id');
         that.setData({
-          user: user,
-          invest: invest,
-          resource: resource,
-          project_info: project_info,
-          invest_case: invest_case,
-          button_type:button_type
+            user_id: user_id,
         })
-      },
-      fail: function (res) {
-        console.log(res)
-      },
-    })
-  },
-  //进入个人详情
-  userInfo: function () {
-    wx.navigateTo({
-      url: "/pages/userDetail/networkDetail/networkDetail"
-    })
-  },
-  // 好友直接拨打电话
-  telephone: function (e) {
-    var telephone = e.currentTarget.dataset.telephone;
-    var tel = telephone.indexOf("****") * 1;
+        //分享至群打点准备
+        wx.showShareMenu({
+            withShareTicket: true,
+        })
 
-    if (tel == -1) {
-      wx.makePhoneCall({
-        phoneNumber: telephone,
-      })
-    } else {
-
-    }
-
-  },
-  //添加人脉
-  addNetWork: function () {
-    var that = this;
-    var user_id = wx.getStorageSync('user_id');//获取我的user_id
-    var followed_user_id = this.data.user_id;//当前用户的
-    console.log(user_id, followed_user_id);
-    if (user_id == false) {
-      wx.showModal({
-        title: "提示",
-        content: "请先绑定个人信息",
-        success: function (res) {
-          wx.setStorageSync('followed_user_id', followed_user_id)
-          if (res.confirm == true) {
-            wx.navigateTo({
-              url: '/pages/register/personInfo/personInfo'
-            })
-          }
-        }
-      })
-    } else if (user_id != false) {
-      wx.request({
-        url: url + '/api/user/followUser',
-        data: {
-          follow_user_id: user_id,
-          followed_user_id: followed_user_id
-        },
-        method: 'POST',
-        success: function (res) {
-          console.log(res)
-          if (res.data.status_code == 2000000) {
-            wx.showModal({
-              title: "提示",
-              content: "添加人脉成功",
-              showCancel: false,
-              confirmText: "到人脉库",
-              success: function (res) {
-                wx.switchTab({
-                  url: '/pages/contacts/contacts/contacts',
+        //用戶的个人信息
+        wx.request({
+            url: url + '/api/user/getUserAllInfo',
+            data: {
+                share_id: 0,
+                user_id: user_id,
+                view_id: view_id,
+            },
+            method: 'POST',
+            success: function (res) {
+                console.log(res)
+                var user = res.data.user_info;
+                var invest = res.data.invest_info;
+                var resource = res.data.resource_info;
+                var project_info = res.data.project_info;
+                var invest_case = res.data.invest_case;
+                var tel = user.user_mobile;
+                var button_type = res.data.button_type;
+                if (button_type == 0) {
+                    console.log("是好友或者是我")
+                } else if (button_type == 1) {
+                    console.log("我分享出去的名片")
+                } else if (button_type == 2) {
+                    console.log("正常添加方式")
+                }
+                if (tel.indexOf("*") != -1) {
+                    that.setData({
+                        blue: 1
+                    })
+                }
+                // console.log(that.data.blue)
+                that.setData({
+                    user: user,
+                    invest: invest,
+                    resource: resource,
+                    project_info: project_info,
+                    invest_case: invest_case,
+                    button_type: button_type
                 })
-              }
+            },
+            fail: function (res) {
+                console.log(res)
+            },
+        })
+    },
+    //进入个人详情
+    userInfo: function () {
+        wx.navigateTo({
+            url: "/pages/userDetail/networkDetail/networkDetail"
+        })
+    },
+    // 好友直接拨打电话
+    telephone: function (e) {
+        var telephone = e.currentTarget.dataset.telephone;
+        var tel = telephone.indexOf("****") * 1;
+
+        if (tel == -1) {
+            wx.makePhoneCall({
+                phoneNumber: telephone,
             })
-          } else {
+        } else {
+
+        }
+
+    },
+    //添加人脉
+    addNetWork: function () {
+        var that = this;
+        var user_id = wx.getStorageSync('user_id');//获取我的user_id
+        var followed_user_id = this.data.user_id;//当前用户的
+        console.log(user_id, followed_user_id);
+        if (user_id == false) {
             wx.showModal({
-              title: "提示",
-              content: "您已经添加过此人脉",
-              showCancel: false,
-              confirmText: "到人脉库",
-              success: function () {
-                wx.switchTab({
-                  url: '/pages/contacts/contacts/contacts',
-                })
-              }
+                title: "提示",
+                content: "请先绑定个人信息",
+                success: function (res) {
+                    wx.setStorageSync('followed_user_id', followed_user_id)
+                    if (res.confirm == true) {
+                        wx.navigateTo({
+                            url: '/pages/register/personInfo/personInfo'
+                        })
+                    }
+                }
             })
-          }
-        },
-        fail: function (res) {
-          wx.showModal({
-            title: "错误提示",
-            content: "添加人脉失败" + res
-          })
-        },
-      })
-    } else {
-      showModal({
-        title: "错误提示",
-        content: "bindUser部分出问题了"
-      })
-    }
-  },
-  addPerson: function (options) {
-    var that = this;
-    var followed_user_id = this.data.user_id;//当前用户的
-    let view_id = wx.getStorageSync('user_id');//获取我自己的user_id/查看者的id
-    let button_type = this.data.button_type;
-    console.log(button_type)
-    if(button_type == 1){
-      wx.request({
-        url: url + '/api/user/followUser',
-        data: {
-          follow_user_id: user_id,
-          followed_user_id: followed_user_id
-          },
-        method: 'POST',
-        success: function (res) {
-          // console.log("button_type=1")
+        } else if (user_id != false) {
+            wx.request({
+                url: url + '/api/user/followUser',
+                data: {
+                    follow_user_id: user_id,
+                    followed_user_id: followed_user_id
+                },
+                method: 'POST',
+                success: function (res) {
+                    console.log(res)
+                    if (res.data.status_code == 2000000) {
+                        wx.showModal({
+                            title: "提示",
+                            content: "添加人脉成功",
+                            showCancel: false,
+                            confirmText: "到人脉库",
+                            success: function (res) {
+                                wx.switchTab({
+                                    url: '/pages/contacts/contacts/contacts',
+                                })
+                            }
+                        })
+                    } else {
+                        wx.showModal({
+                            title: "提示",
+                            content: "您已经添加过此人脉",
+                            showCancel: false,
+                            confirmText: "到人脉库",
+                            success: function () {
+                                wx.switchTab({
+                                    url: '/pages/contacts/contacts/contacts',
+                                })
+                            }
+                        })
+                    }
+                },
+                fail: function (res) {
+                    wx.showModal({
+                        title: "错误提示",
+                        content: "添加人脉失败" + res
+                    })
+                },
+            })
+        } else {
+            showModal({
+                title: "错误提示",
+                content: "bindUser部分出问题了"
+            })
         }
-      })
-    }else if(button_type ==2){
-      wx.request({
-        url: url + '/api/user/UserApplyFollowUser',
-        data: {
-          user_id: followed_user_id,
-          applied_user_id: view_id
-        },
-        method: 'POST',
-        success: function (res) {
-          console.log(res)
+    },
+    //添加人脉
+    addPerson: function (options) {
+        var that = this;
+        var followed_user_id = this.data.user_id;//当前用户的
+        let view_id = wx.getStorageSync('user_id');//获取我自己的user_id/查看者的id
+        let button_type = this.data.button_type;
+        console.log(button_type)
+        if (button_type == 1) {
+            wx.request({
+                url: url + '/api/user/followUser',
+                data: {
+                    follow_user_id: user_id,
+                    followed_user_id: followed_user_id
+                },
+                method: 'POST',
+                success: function (res) {
+                    that.setData({
+                        a: 2
+                    })
+                }
+            })
+        } else if (button_type == 2) {
+            wx.request({
+                url: url + '/api/user/UserApplyFollowUser',
+                data: {
+                    user_id: followed_user_id,
+                    applied_user_id: view_id
+                },
+                method: 'POST',
+                success: function (res) {
+                    that.setData({
+                        a: 2
+                    })
+                    console.log(res)
+                }
+            })
         }
-      })
+
+    },
+    //分享页面
+    onShareAppMessage: function () {
+        var user_id = this.data.user_id;
+        var share_id = wx.getStorageSync("user_id");
+        return app.sharePage(user_id, share_id)
     }
-  
-  },
-  //分享页面
-  onShareAppMessage: function () {
-    var user_id = this.data.user_id;
-    var share_id=wx.getStorageSync("user_id");
-    return app.sharePage(user_id,share_id)
-  }
 });
