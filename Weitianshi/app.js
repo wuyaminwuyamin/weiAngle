e487519c4c88db5d2a1004ab6eb409b4aceae0d0
youmm <youmm@tongdaohui.com>
2017-06-21 11:33:34 +0800

    阮千军
    1.正式上线

--- Weitianshi/app.js
+++ Weitianshi/app.js
@@ -126,7 +126,7 @@ App({
                         that.globalData.encryptedData = res.encryptedData;
                         that.globalData.iv = res.iv;
                         wx.request({
-                            url: 'https://wx.debug.weitianshi.cn/api/wx/returnOauth',
+                            url: 'https://wx.weitianshi.cn/api/wx/returnOauth',
                             data: {
                                 code: code,
                                 encryptedData: res.encryptedData,
@@ -153,7 +153,7 @@ App({
                     //用户不授权
                     fail: function (res) {
                         wx.request({
-                            url: 'https://wx.debug.weitianshi.cn/api/wx/returnOauth',
+                            url: 'https://wx.weitianshi.cn/api/wx/returnOauth',
                             data: {
                                 code: code,
                             },
@@ -440,7 +440,7 @@ App({
     //初始本地缓存
     globalData: {
         error: 0,
-        url: "https://wx.debug.weitianshi.cn",
-        url_common: "https://debug.weitianshi.cn"
+        url: "https://wx.weitianshi.cn",
+        url_common: "https://weitianshi.cn"
     }
 });
