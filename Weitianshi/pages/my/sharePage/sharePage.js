f86d52b95ef033c0182feddd5acf951b269e1ef8
youmm <youmm@tongdaohui.com>
2017-06-21 15:51:56 +0800

    阮千军
    1.修复了分享名片时未注册用户添加的情景

--- Weitianshi/pages/my/sharePage/sharePage.js
+++ Weitianshi/pages/my/sharePage/sharePage.js
@@ -47,7 +47,7 @@ Page({
                         invest_case: invest_case,
                         button_type: button_type,
                         count: count,
-                        view_id:view_id
+                        view_id: view_id
                     })
                     if (button_type == 0) {
                         console.log("申請添加人脈")
@@ -56,9 +56,9 @@ Page({
                     } else if (button_type == 2) {
                         console.log("待验证")
                     } else if (button_type == 3) {
-                      console.log("同意加人脉")
+                        console.log("同意加人脉")
                     } else if (button_type == 4) {
-                      console.log("单方加人脉")
+                        console.log("单方加人脉")
                     }
                     wx.setNavigationBarTitle({
                         title: res.data.user_info.user_real_name + "的投资名片",
@@ -68,7 +68,7 @@ Page({
                     console.log(res)
                 },
             })
-            console.log("分享","用户","查看的人")
+            console.log("分享者", "数据显示的人", "查看的人")
             console.log(share_id, followed_user_id, view_id)
 
             //如果进入的是自己的名片里
@@ -90,14 +90,78 @@ Page({
         var followed_user_id = this.data.followed_user_id;//当前被查看的用户id;
         let button_type = this.data.button_type;
         var view_id = this.data.view_id;
-        console.log("当前查看的人")
-        console.log(view_id)
-        // button_type==0  0申请加人脉按钮，1不显示任何按钮  2待验证   3同意加为人脉  4加为单方人脉
+        // button_type==0  0.申请加人脉按钮 1.不显示任何按钮  2.待验证   3.同意加为人脉  4.加为单方人脉
 
         console.log(button_type)
-
         //直接可添加好友的情况
-        if (button_type == 4) {
+        if (button_type == 0) {
+            //走正常申请流程
+            wx.request({
+                url: url + '/api/user/checkUserInfo',
+                data: {
+                    user_id: user_id
+                },
+                method: 'POST',
+                success: function (res) {
+                    if (res.data.status_code == 2000000) {
+                        var complete = res.data.is_complete;
+                        if (complete == 1) {
+                            //如果信息完整就正常申请添加人脉
+                            wx.request({
+                                url: url + '/api/user/UserApplyFollowUser',
+                                data: {
+                                    user_id: user_id,
+                                    applied_user_id: followed_user_id
+                                },
+                                method: 'POST',
+                                success: function (res) {
+                                    console.log("这里是正常申请添加人脉")
+                                    console.log(res)
+                                    that.setData({
+                                        button_type: 2
+                                    })
+                                }
+                            })
+                        } else if (complete == 0) {
+                            //如果有user_id但信息不全则跳companyInfo页面
+                            wx.setStorageSync('followed_user_id', followed_user_id)
+                            wx.navigateTo({
+                                url: '/pages/register/companyInfo/companyInfo'
+                            })
+                        }
+                    } else {
+                        //如果没有user_id则跳personInfo
+                        wx.setStorageSync('followed_user_id', followed_user_id)
+                        wx.navigateTo({
+                            url: '/pages/register/personInfo/personInfo'
+                        })
+                    }
+                },
+            });
+        } else if (button_type == 1) {
+            console.log("互為好友或單方人脈")
+        } else if (button_type == 2) {
+            console.log("待驗證")
+        } else if (button_type == 3) {
+            wx.request({
+                url: url + '/api/user/handleApplyFollowUser',
+                data: {
+                    // 当前登录者的
+                    user_id: user_id,
+                    // 当前申请的用户
+                    apply_user_id: followed_user_id
+                },
+                method: 'POST',
+                success: function (res) {
+                    console.log(res)
+                    console.log("同意加為人脈")
+                    that.setData({
+                        button_type: 1
+                    })
+                }
+            })
+        } else if (button_type == 4) {
+            wx.setStorageSync('followed_user_id', followed_user_id)
             wx.setStorageSync("driectAdd", 1)
             //判断用户信息是否完整
             wx.request({
@@ -111,7 +175,7 @@ Page({
                         var complete = res.data.is_complete;
 
                         if (complete == 1) {
-                           //如果信息完整就直接添加人脉
+                            //如果信息完整就直接添加人脉
                             wx.request({
                                 url: url + '/api/user/followUser',
                                 data: {
@@ -141,77 +205,10 @@ Page({
                     }
                 },
             });
-        } else if (button_type == 0) {
-            //走正常申请流程
-            wx.request({
-                url: url + '/api/user/checkUserInfo',
-                data: {
-                    user_id: user_id
-                },
-                method: 'POST',
-                success: function (res) {
-                    if (res.data.status_code == 2000000) {
-                        var complete = res.data.is_complete;
-
-                        if (complete == 1) {
-                            //如果信息完整就正常申请添加人脉
-                            wx.request({
-                                url: url + '/api/user/UserApplyFollowUser',
-                                data: {
-                                    user_id: user_id,
-                                    applied_user_id: followed_user_id
-                                },
-                                method: 'POST',
-                                success: function (res) {
-                                    console.log("这里是正常申请添加人脉")
-                                    console.log(res)
-                                    that.setData({
-                                        button_type: 2
-                                    })
-                                }
-                            })
-                        } else if (complete == 0) {
-                            //如果有user_id但信息不全则跳companyInfo页面
-                            wx.navigateTo({
-                                url: '/pages/register/companyInfo/companyInfo'
-                            })
-                        }
-                    } else {
-                        //如果没有user_id则跳personInfo
-                        wx.navigateTo({
-                            url: '/pages/register/personInfo/personInfo'
-                        })
-                    }
-                },
-            });
-        }else if(button_type == 1){
-          console.log("互為好友或單方人脈")
-        } else if(button_type ==2){
-          console.log("待驗證")
-        } else if (button_type == 3){    
-          wx.request({
-            url: url + '/api/user/handleApplyFollowUser',
-            data: {
-              // 当前登录者的
-              user_id: user_id,
-              // 当前申请的用户
-              apply_user_id: followed_user_id
-            },
-            method: 'POST',
-            success: function (res) {
-              console.log(res)
-              console.log("同意加為人脈")
-              that.setData({
-                button_type: 1
-              })
-            }
-          })
-
-        }
-        else {
+        } else {
             showModal({
                 title: "错误提示",
-                content: "button_type为"+button_type
+                content: "button_type为" + button_type
             })
         }
     },
@@ -229,7 +226,7 @@ Page({
     //分享页面部分
     onShareAppMessage: function () {
         var user_id = this.data.followed_user_id;
-        var share_id=this.data.share_id;
-        return app.sharePage(user_id,share_id)
+        var share_id = this.data.share_id;
+        return app.sharePage(user_id, share_id)
     }
 }); 
