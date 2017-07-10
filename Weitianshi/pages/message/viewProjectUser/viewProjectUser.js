eb80c8ca8aff51a2e0db9a2b38c59625eee04d4a
lovefruit1314 <521fruit.love@163.com>
2017-07-10 17:59:33 +0800

    霍春阳
    增加 浏览我的内容

--- Weitianshi/pages/message/viewProjectUser/viewProjectUser.js
+++ Weitianshi/pages/message/viewProjectUser/viewProjectUser.js
@@ -1,66 +1,143 @@
 // pages/message/viewProjectUser/viewProjectUser.js
+var rqj = require('../../Template/Template.js')
+var app = getApp();
+var url = app.globalData.url;
+var url_common = app.globalData.url_common;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    count: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-  
-  },
+    var that = this;
+    app.initPage(that);
+    var user_id = this.data.user_id;
+    let project_id = "ypOVDKWK";
+    // 获取浏览我的用户信息
+    if (user_id) {
+      wx.request({
+        url: url_common + '/api/message/viewProjectUserList',
+        data: {
+          user_id: user_id,
+          project_id: project_id,
+          type_id: 6
+        },
+        method: 'POST',
+        success: function (res) {
+          console.log("浏览了我的用户的数据")
+         
+          var contacts = res.data.list.users;
+          console.log(contacts)
+          var count = res.data.list.count;
+          var page_end = res.data.page_end;
+          that.setData({
+            contacts: contacts,
+            page_end: page_end,
+            count: count
+          })
+        }
+      })
+    }
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+    //向后台发送信息取消红点
+    wx.request({
+      url: url_common + '/api/message/setMessageToRead',
+      data: {
+        user_id: user_id,
+        type_id: 3
+      },
+      method: "POST",
+    })
   },
+  // 添加人脉
+  addNetWork: function (e) {
+    var that = this;
+    var user_id = wx.getStorageSync('user_id');//获取我的user_id
+    var followed_user_id = e.target.dataset.followedid;//当前用户的user_id
+    var follow_status = e.currentTarget.dataset.follow_status;
+    var index = e.target.dataset.index;
+    var contacts = this.data.contacts
+    console.log(contacts)
+    console.log(user_id, followed_user_id);
+    if (follow_status == 0) {
+      //添加人脉接口
+      wx.request({
+        url: url + '/api/user/UserApplyFollowUser',
+        data: {
+          user_id : user_id,
+          applied_user_id: followed_user_id
+        },
+        method: 'POST',
+        success: function (res) {
+          console.log(res)
+          if (res.data.status_code == 2000000) {
+            //将状态设为"未验证"
+            contacts.forEach((x) => {
+              if (x.user_id == followed_user_id) {
+                x.follow_status = 2
+              }
+            })
+            that.setData({
+              contacts: contacts
+            })
+          }
+        },
+        fail: function (res) {
+          wx.showModal({
+            title: "错误提示",
+            content: "添加人脉失败" + res
+          })
+        },
+      })
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+    } else if (follow_status == 3) {
+      // 同意申請接口
+      wx.request({
+        url: url + '/api/user/handleApplyFollowUser',
+        data: {
+          user_id: user_id,
+          apply_user_id: followed_user_id
+        },
+        method: 'POST',
+        success: function (res) {
+          console.log(res)
+          console.log("同意申請")
+          if (res.data.status_code == 2000000) {
+            //将状态设为"未验证"
+            contacts.forEach((x) => {
+              if (x.user_id == followed_user_id) {
+                x.follow_status = 1
+              }
+            })
+            that.setData({
+              contacts: contacts
+            })
+          }
+        }
+      })
+    }
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
+  // 用户详情
+  userDetail: function (e) {
+    var id = e.currentTarget.dataset.id
+    console.log(id);
+    wx.navigateTo({
+      url: '/pages/userDetail/networkDetail/networkDetail?id=' + id,
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+  //下拉加载
+  loadMore: function () {
+    var that = this;
+    var user_id = this.data.user_id
+    var currentPage = this.data.currentPage;
+    var request = {
+      url: url_common + '/api/message/viewCardMessage',
+      data: {
+        user_id: user_id,
+        page: currentPage,
+        type_id: 3
+      }
+    }
+    //调用通用加载函数
+    app.loadMore(that, request, "contacts", that.data.contacts)
   },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
 })
