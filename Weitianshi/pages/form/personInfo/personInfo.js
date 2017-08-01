a037836dc32491d5ac03b3aea3caa429bb749532
lovefruit1314 <521fruit.love@163.com>
2017-08-01 16:01:14 +0800

    霍春阳
    推送项目

--- Weitianshi/pages/form/personInfo/personInfo.js
+++ Weitianshi/pages/form/personInfo/personInfo.js
@@ -13,6 +13,7 @@ Page({
     let user_mobile = options.mobile;
     let user_brand = options.brand;
     let user_company = options.company;
+    console.log(user_company)
     let user_career = options.career;
     let user_email = options.email;
     let type = options.type;
@@ -55,9 +56,49 @@ Page({
       user_name: user_name
     })
   },
+  telEdit: function (e) {
+    let user_tel = e.detail.value;
+    console.log(user_tel)
+    this.setData({
+      user_tel: user_tel
+    })
+  },
+  brandEdit: function (e) {
+    let user_brand = e.detail.value;
+    console.log(user_brand)
+    this.setData({
+      user_brand: user_brand
+    })
+  },
+  companyEdit: function (e) {
+    let user_company_name = e.detail.value;
+    console.log(user_company_name)
+    this.setData({
+      user_company_name: user_company_name
+    })
+  },
+  careerEdit: function (e) {
+    let user_career = e.detail.value;
+    console.log(user_career)
+    this.setData({
+      user_career: user_career
+    })
+  },
+  emailEdit: function (e) {
+    let user_email = e.detail.value;
+    console.log(user_email)
+    this.setData({
+      user_email: user_email
+    })
+  },
   save: function () {
     let user_id = wx.getStorageSync('user_id');
     let user_name = this.data.user_name;
+    let user_tel = this.data.user_tel;
+    let user_brand = this.data.user_brand;
+    let user_company_name = this.data.user_company_name;
+    let user_career = this.data.user_career;
+    let user_email = this.data.user_email;
     let type = this.data.type;
     // 姓名type:0 手机type:1 品牌type:2 公司type:3 职位type:4 邮箱type:5
     if (type == 0) {
@@ -66,23 +107,23 @@ Page({
       })
     } else if (type == 1){
       wx.navigateTo({
-        url: '/pages/my/identity/identityEdit/identityEdit?user_name=' + user_name,
+        url: '/pages/my/identity/identityEdit/identityEdit?user_tel=' + user_tel,
       })
     } else if (type == 2) {
       wx.navigateTo({
-        url: '/pages/my/identity/identityEdit/identityEdit?user_name=' + user_name,
+        url: '/pages/my/identity/identityEdit/identityEdit?user_brand=' + user_brand,
       })
     } else if (type == 3) {
       wx.navigateTo({
-        url: '/pages/my/identity/identityEdit/identityEdit?user_name=' + user_name,
+        url: '/pages/my/identity/identityEdit/identityEdit?user_company_name=' + user_company_name,
       })
     } else if (type == 4) {
       wx.navigateTo({
-        url: '/pages/my/identity/identityEdit/identityEdit?user_name=' + user_name,
+        url: '/pages/my/identity/identityEdit/identityEdit?user_career=' + user_career,
       })
     } else if (type == 5) {
       wx.navigateTo({
-        url: '/pages/my/identity/identityEdit/identityEdit?user_name=' + user_name,
+        url: '/pages/my/identity/identityEdit/identityEdit?user_email=' + user_email,
       })
     }
 
--- Weitianshi/pages/myProject/pushTo/pushTo.js
+++ Weitianshi/pages/myProject/pushTo/pushTo.js
@@ -12,35 +12,131 @@ Page({
 
   onLoad: function (options) {
     // pushed_user_id == 推送给谁
+    let pushed_user_id = options.pushId;
+    console.log(pushed_user_id)
+    let that = this;
+    that.setData({
+      pushed_user_id: pushed_user_id
+    })
+   
+  },
+  onShow: function () {
+    var that = this;
+    app.initPage(that);
+    let user_id = wx.getStorageSync('user_id');
+    let pushed_user_id = this.data.pushed_user_id;
+    that.setData({
+      user_id: user_id,
+      scroll: 0,
+      requestCheck: true,
+      currentPage: 1,
+      page_end: false,
+    })
     wx.request({
       url: url_common + '/api/project/getMyProjectList',
       data: {
-        user_id: "90ky197p",
-        type:'push',
-        pushed_user_id:"Er6NwnMr"
+        user_id: user_id,
+        type: 'push',
+        pushed_user_id: pushed_user_id,
+        page: 1
       },
       method: 'POST',
       success: function (res) {
         console.log(res)
+        let contacts = res.data.data;
+        let pushTimes = res.data.push_times;
+        var page_end = res.data.page_end;
+        contacts.forEach((x, index) => {
+          contacts[index] = x;
+        })
+        // is_exclusive是否独家 1独家 2非独家 0其他
+        that.setData({
+          contacts: contacts,
+          pushTimes: pushTimes,
+          pushed_user_id: pushed_user_id
+        })
       }
     })
   },
-  onShow: function () {
-
-  },
-  pushTo: function () {
-    wx.showToast({
-      title: '成功',
-      icon: 'success',
-      duration: 2000
-    })
-    //  错误信息提示
-    rqj.errorHide(that, "最多可选择五项", 1000)
-  },
+ 
   // 创建项目
   createProject: function () {
     wx.navigateTo({
       url: '/pages/myProject/publishProject/publishProject',
     })
-  }
+  },
+  // 选中项目
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+    let checkBoxId = e.detail.value;
+    let checkArray = [];
+    checkArray.push(checkBoxId);
+    console.log(checkArray)
+
+
+    
+  },
+  pushTo: function () {
+    let user_id = wx.getStorageSync('user_id');
+    let pushed_user_id = this.data.pushed_user_id;
+    let time = this.data.pushTimes;
+    let that = this;
+    wx.request({
+      url: url + '/api/project/pushProjectToUser',
+      data: {
+        user_id: user_id,
+        pushed_user_id: "90ky197p",
+        pushed_project: ['xpEv2arq']
+      },
+      method: 'POST',
+      success: function (res) {
+        console.log(res)
+        wx.request({
+          url: url_common + '/api/project/getMyProjectList',
+          data: {
+            user_id: user_id,
+            type: 'push',
+            pushed_user_id: pushed_user_id
+          },
+          method: 'POST',
+          success: function (res) {
+            console.log(res)
+            let dataList = res.data.data;
+            let pushTimes = res.data.push_times;
+            that.setData({
+              pushTimes: pushTimes
+            })
+            console.log(pushTimes)
+          }
+        })
+        wx.showToast({
+          title: '成功',
+          icon: 'success',
+          duration: 2000
+        })
+      }
+    })
+
+    //  错误信息提示
+    // rqj.errorHide(that, "最多可选择五项", 1000)
+  },
+  //上拉加载
+  loadMore: function () {
+    //请求上拉加载接口所需要的参数
+    console.log("yes")
+    var that = this;
+    var user_id = wx.getStorageSync('user_id');
+    var currentPage = this.data.currentPage;
+    var request = {
+      url: url_common + '/api/project/getMyProjectList',
+      data: {
+        user_id: user_id,
+        page: this.data.currentPage,
+        type: 'push',
+        pushed_user_id: pushed_user_id
+      }
+    }
+    //调用通用加载函数
+    app.loadMore(that, request, "contacts", that.data.contacts)
+  },
 })
